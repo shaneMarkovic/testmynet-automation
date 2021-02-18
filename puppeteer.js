df6fc5593aee5cc55b40a43a76e54e1ae1130d6e
shaneMarkovic <dusan.shane.markovic@gmail.com>
2021-02-18 18:05:28 +0100

    enable sandbox

--- puppeteer.js
+++ puppeteer.js
@@ -3,7 +3,7 @@ const useProxy = require('puppeteer-page-proxy');
 
 (async () => {
     try {
-        const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+        const browser = await puppeteer.launch({ headless: true });
         console.log("Launched.")
         const page = await browser.newPage();
         console.log("Opened new page");
