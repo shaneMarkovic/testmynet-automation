5092154f3688896e634879f4db5497da32163e7e
shaneMarkovic <dusan.shane.markovic@gmail.com>
2021-02-19 21:33:50 +0100

    test

--- puppeteer.js
+++ puppeteer.js
@@ -2,22 +2,28 @@ const puppeteer = require('puppeteer');
 const useProxy = require('puppeteer-page-proxy');
 
 async function testSpeed(username, password, ip, port) {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: false, args: [ '--proxy-server=http://157.230.184.117:8225' ] });
     let dwSpeed = 0;
     let upSpeed = 0;
     console.log("Launched.")
     try {
         const page = await browser.newPage();
+        await page.authenticate({
+            username: "admin",
+            password: "mhZdFu7H2R",
+        });
         console.log("Opened new page");
         page.setDefaultNavigationTimeout(0);
-        await useProxy(page, `http://${username}:${password}@${ip}:${port}`);
+        // await useProxy(page, `socks5://${ip}:${port}`);
         console.log("Connected to proxy.");
         await page.goto('https://testmy.net/mirror?testServer=fl');
+        
         console.log("Selected mirror florida");
         await page.goto('https://testmy.net/download');
         console.log("Opened downlaod test");
         await page.select('select[name="s"]', '25MB');
-        await page.waitForNavigation({ timeout: 0, waitUntil: "networkidle0" });
+        console.log("Download started")
+        await page.waitForNavigation({ timeout: 0, waitUntil: "networkidle2" });
         const dwSiblings = await page.$$('.im-dnarw');
         const dwSibling = dwSiblings[0];
         const dwParent = (await dwSibling.$x('..'))[0]; // Element Parent
@@ -29,6 +35,7 @@ async function testSpeed(username, password, ip, port) {
         await page.goto('https://testmy.net/upload');
         console.log("Opened upload test");
         await page.select('select[name="s"]', '25MB');
+        console.log("Upload started")
         await page.waitForNavigation({ timeout: 0, waitUntil: "networkidle0" });
         const upSiblings = await page.$$('.im-uparw');
         const upSibling = upSiblings[0];
@@ -38,9 +45,9 @@ async function testSpeed(username, password, ip, port) {
         const upSpeedFull = upParentHtml.replace(`JSHandle:<span class="icoImg im-uparw" aria-hidden="true"></span> <span class="color23">`, "").replace("</span>").split(" ");
         upSpeed = upSpeedFull[0];
         console.log(upSpeed);
-        browser.close();
+        // browser.close();
     } catch(e) {
-        browser.close();
+        // browser.close();
         console.log(e);
         throw new Error(`Check logs for errors, Download: ${dwSpeed}Mbps Upload: ${upSpeed}Mbps`);
     }
