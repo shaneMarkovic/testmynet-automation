8ec2eb494a1f8f19ec68f7a7ed600a6aee07924f
shaneMarkovic <dusan.shane.markovic@gmail.com>
2021-03-02 22:22:05 +0100

    update configs

--- automatic.js
+++ automatic.js
@@ -4,20 +4,7 @@ const axios = require('axios');
 
 async function testSpeed(id, username, password, ip, port, mirror) {
     const browser = await puppeteer.launch({ headless: true, args: [`--proxy-server=http://${ip}:${port}`,
-    '--disable-canvas-aa', // Disable antialiasing on 2d canvas
-    '--disable-2d-canvas-clip-aa', // Disable antialiasing on 2d canvas clips
-    // '--disable-gl-drawing-for-tests', // BEST OPTION EVER! Disables GL drawing operations which produce pixel output. With this the GL output will not be correct but tests will run faster.
-    '--disable-dev-shm-usage', // ???
-    '--no-zygote', // wtf does that mean ?
-    '--use-gl=swiftshader', // better cpu usage with --use-gl=desktop rather than --use-gl=swiftshader, still needs more testing.
-    '--enable-webgl',
-    '--hide-scrollbars',
-    '--mute-audio',
     '--no-first-run',
-    '--disable-infobars',
-    '--disable-breakpad',
-    //'--ignore-gpu-blacklist',
-    '--window-size=1280,1024', // see defaultViewport
     '--no-sandbox', // meh but better resource comsuption
     '--disable-setuid-sandbox'] });
     let dwSpeed = 0;
@@ -30,7 +17,7 @@ async function testSpeed(id, username, password, ip, port, mirror) {
             password,
         });
         console.log("Authenticated");
-        await page.setDefaultNavigationTimeout(0);
+        await page.setDefaultNavigationTimeout(60000);
         await page.goto(`https://${mirror}.testmy.net`);
         await page.$eval('#testBtnMn', el => el.click());
         await page.click('a[title="Upload & Download Speed Test"]');
